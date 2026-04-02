e86fa273e420c798f61025a5a90e0237f5fa8b0c
Agampreet Singh <73707055+Agampreet-Singh@users.noreply.github.com>
2026-04-02 17:00:31 +0530

    Create __main__.py

---
+++ redrootsqli/__main__.py
@@ -0,0 +1,14 @@
+# ==================================================
+# Project: RedRoot
+# Author: Agampreet Singh
+# Copyright (c) 2026 Agampreet Singh
+# License: Proprietary – All Rights Reserved
+#
+# Unauthorized copying, modification, distribution,
+# or use of this file is strictly prohibited.
+# ==================================================
+
+from redrootsqli.cli import app
+
+if __name__ == "__main__":
+    app()
